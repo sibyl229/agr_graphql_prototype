df9e0ef182e8fa8d32f17e78c73005900dd3f6d0
Sibyl <sibyl@wormbase.org>
2018-10-09 16:51:08 -0400

    add gene-species association

--- src/schema.js
+++ src/schema.js
@@ -4,6 +4,12 @@ export const typeDefs = `
 type Gene {
   primaryKey: ID!,
   symbol: String,
+  species: Species @relation(name: "FROM_SPECIES", direction: "OUT")
+}
+
+type Species {
+  name: String,
+  primaryKey: ID!,
 }
 
 type Query {
